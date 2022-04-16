3f406b146de97e8ab9ebe096da8009259d691b70
thinh.hoang <thinh.hoang@thlgroup.vn>
2022-04-16 10:02:12 +0700

    real

--- db.js
+++ db.js
@@ -1,13 +1,8 @@
-const items = require("./data/items");
-const tags = require("./data/tags");
-const brands = require("./data/brands");
-const itemTypes = require("./data/itemTypes");
+const recipe = require("./data/recipe");
 
 module.exports = {
-  items,
-  tags,
-  brands,
-  itemTypes
+ 
+  recipe
 }
 
 
